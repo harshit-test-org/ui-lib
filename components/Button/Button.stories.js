980bb2d328dc66e147f7563c242a66156f80463b
Harshit Pant <pantharshit00@gmail.com>
2018-05-25 20:36:54 +0530

    build: finalize build and publishing process

--- components/Button/Button.stories.js
+++ components/Button/Button.stories.js
@@ -4,6 +4,6 @@ import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
 // import { linkTo } from '@storybook/addon-links'
 
-import { Button } from './'
+import Button from './'
 
-storiesOf('Button', module).add('with somet text test', () => <Button>Hell Button</Button>)
+storiesOf('Button', module).add('with somet text test', () => <Button>Hello Button</Button>)
