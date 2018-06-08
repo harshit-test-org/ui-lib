a5a88eab5c607f5f060544ee30fcd5f9aef5dd94
Harshit Pant <pantharshit00@gmail.com>
2018-06-08 16:44:39 +0530

    Add normalize css to storybook

--- components/index.js
+++ components/index.js
@@ -7,3 +7,7 @@ export {
   CardContent,
   CardHeader
 } from './Card'
+export { default as Blanket } from './Blanket'
+export { default as Spinner } from './Spinner'
+export { default as Appbar } from './Appbar'
+export { default as Drawer } from './Drawer'
