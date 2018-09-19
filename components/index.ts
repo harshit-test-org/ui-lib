76dc98cceca6e8f19847ad254c42c4cea42ef115
Harshit Pant <pantharshit00@gmail.com>
2018-09-19 22:43:00 +0530

    fix: remove atlaskit exports

--- components/index.ts
+++ components/index.ts
@@ -3,8 +3,6 @@ export { default as Typography } from './Typography';
 export { default as baseTheme } from './theme';
 export { default as shadows } from './utils/shadows';
 export { default as Card, CardActions, CardContent, CardHeader } from './Card';
-export { default as Blanket } from './Blanket';
-export { default as Spinner } from './Spinner';
 export { default as Appbar } from './Appbar';
 export { default as Drawer } from './Drawer';
 export { default as CardHorizontalScroller } from './CardHorizontalScroller';
